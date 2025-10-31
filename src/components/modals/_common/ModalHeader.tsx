80c52d1a54d949f334724ff7bf844842883366ec
heesu <phis3210@naver.com>
2025-10-31 17:58:46 +0900

    feat: 공용 Modal Header 컴포넌트 생성

---
+++ src/components/modals/_common/ModalHeader.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from "react";
+
+import icon_close from "../../../assets/icon_close.png";
+
+import "./ModalHeader.css";
+
+const ModalHeader = ({
+  theme = "default",
+  children,
+}: {
+  theme?: "default" | "orange";
+  children: ReactNode;
+}) => {
+  return <div className={`modal-header theme-${theme}`}>{children}</div>;
+};
+
+const CloseButton = ({ onCloseClick }: { onCloseClick: () => void }) => {
+  return (
+    <img
+      src={icon_close}
+      alt="modal close button"
+      className="close-button"
+      onClick={onCloseClick}
+    />
+  );
+};
+
+ModalHeader.CloseButton = CloseButton;
+
+export default ModalHeader;
