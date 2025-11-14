330b0f67db062693cb83bcfcb4ac61a93973d75c
heesu <phis3210@naver.com>
2025-11-14 17:16:43 +0900

    feat: 모달 애니메이션 기능 추가 - react-transition-group 설치

--- src/components/modals/ConfirmModal/ConfirmModal.tsx
+++ src/components/modals/ConfirmModal/ConfirmModal.tsx
@@ -1,14 +1,37 @@
+import { useEffect } from "react";
+
+import useModal from "../../../hooks/useModal";
+import useToggle from "../../../hooks/useToggle";
+
 import GeneralModalLayout from "../_layout/GeneralModalLayout";
 import ModalHeader from "../_common/ModalHeader";
 import ModalFooter from "../_common/ModalFooter";
 import ModalContent from "../_common/ModalContent";
-// TODO : 하나의 모듈에서 가져올 수 있도록 변경 필요
 
 const ConfirmModal = () => {
+  const { closeModal } = useModal("dispatch");
+  const { value: isOpen, setFalse: setClose } = useToggle(true);
+
+  useEffect(() => {
+    let timerId = 0;
+
+    if (!isOpen) {
+      timerId = setTimeout(() => {
+        closeModal("confirmModal");
+      }, 400);
+    }
+
+    return () => {
+      if (!isOpen && timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [isOpen]);
+
   return (
-    <GeneralModalLayout>
+    <GeneralModalLayout isOpen={isOpen}>
       <ModalHeader theme="orange">
-        <ModalHeader.CloseButton onCloseClick={() => {}} />
+        <ModalHeader.CloseButton onCloseClick={setClose} />
       </ModalHeader>
 
       <ModalContent>
@@ -19,12 +42,12 @@ const ConfirmModal = () => {
         <ModalFooter.Button
           theme="confirm"
           text="확 인"
-          onButtonClick={() => {}}
+          onButtonClick={setClose}
         />
         <ModalFooter.Button
           theme="cancel"
           text="취 소"
-          onButtonClick={() => {}}
+          onButtonClick={setClose}
         />
       </ModalFooter>
     </GeneralModalLayout>
--- src/components/modals/_layout/GeneralModalLayout.tsx
+++ src/components/modals/_layout/GeneralModalLayout.tsx
@@ -1,6 +1,22 @@
-import type { ReactNode } from "react";
+import { type ReactNode, useRef } from "react";
+import { Transition } from "react-transition-group";
 
 import "./GeneralModalLayout.css";
+
+const DURATION = 400;
+
+const defaultStyle = {
+  transition: `top ${DURATION}ms cubic-bezier(0.78,-0.06, 0.14, 1.04), transform ${DURATION}ms cubic-bezier(0.78,-0.06, 0.14, 1.04)`,
+  top: "50%",
+  transform: "translateY(50%)",
+};
+
+const transitionStyles = {
+  entering: { top: "0%", transform: "translateY(0%)" },
+  entered: { top: "0%", transform: "translateY(0%)" },
+  exiting: { top: "50%", transform: "translateY(50%)" },
+  exited: { top: "50%", transform: "translateY(50%)" },
+} as any;
 
 /**
  * GeneralModalLayout
@@ -8,12 +24,15 @@ import "./GeneralModalLayout.css";
  * Modal의 공통 Layout을 정의한다(크기, 위치, 배경색, radius)
  */
 const GeneralModalLayout = ({
+  isOpen,
   size = "s",
   children,
 }: {
+  isOpen: boolean;
   size?: "s" | "m" | "l";
   children: ReactNode;
 }) => {
+  const nodeRef = useRef<any>(null);
   const sizePreset = { width: "400px" };
 
   switch (size) {
@@ -31,11 +50,25 @@ const GeneralModalLayout = ({
   }
 
   return (
-    <div className="general-modal-layout">
-      <div style={sizePreset} className="modal-frame">
-        {children}
-      </div>
-    </div>
+    <Transition in={isOpen} nodeRef={nodeRef} timeout={DURATION} appear>
+      {(state: any) => {
+        return (
+          <div className={`general-modal-layout`}>
+            <div
+              ref={nodeRef}
+              style={{
+                ...sizePreset,
+                ...defaultStyle,
+                ...transitionStyles[state],
+              }}
+              className={`modal-frame`}
+            >
+              {children}
+            </div>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
