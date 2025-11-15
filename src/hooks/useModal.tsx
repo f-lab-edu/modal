30c05975b96191c8e1916946a04be00c8e50042f
heesu <phis3210@naver.com>
2025-11-15 18:17:45 +0900

    chore: typescript 보수

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,24 +1,31 @@
 import { useContext } from "react";
 import { cloneDeep } from "lodash";
 
-import { ModalDispatchContext, ModalDataContext } from "../Context/Context";
+import {
+  ModalDispatchContext,
+  ModalDataContext,
+  type TModalData,
+  type TModalDispatch,
+} from "../Context/Context";
 
-const DISPATCH = "dispatch" as const;
-const MODALS = "modals" as const;
+const DISPATCH = "dispatch";
+const MODALS = "modals";
 
-const useModal = (type: typeof DISPATCH | typeof MODALS) => {
+function useModal(type: typeof DISPATCH): TModalDispatch;
+function useModal(type: typeof MODALS): TModalData[];
+function useModal(type: typeof DISPATCH | typeof MODALS) {
   const modalDispatch = useContext(ModalDispatchContext);
   const modalData = useContext(ModalDataContext);
 
   if (type === "dispatch" && modalDispatch) {
-    return cloneDeep(modalDispatch);
+    return modalDispatch as TModalDispatch;
   }
 
   if (type === "modals" && modalData) {
-    return cloneDeep(modalData);
+    return cloneDeep(modalData) as TModalData[];
   }
 
   console.error(`there is no ${type} context`);
-};
+}
 
 export default useModal;
